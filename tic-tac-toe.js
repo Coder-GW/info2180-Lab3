1095d3c79a58bcb362a73bf4053c9dea9d6bc707
Coder-GW <williams.gavril@gmail.com>
2025-10-17 08:56:07 -0500

    Added X and O functionality

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -4,7 +4,22 @@ window.addEventListener("DOMContentLoaded", () => {
     const status = document.getElementById("status");
     const newGameBtn = document.querySelector(".btn");
 
+    let currentPlayer = "X";
+    let gameActive = true;
+
     squares.forEach(square => {
         square.classList.add("square");
+
+        square.addEventListener("click", () => handleMove(square));
     });
+
+    function handleMove(square) {
+        if (!gameActive || square.textContent) return;
+
+        square.textContent = currentPlayer;
+        square.classList.add(currentPlayer);
+
+        currentPlayer = currentPlayer === "X" ? "O" : "X";
+    }
+
 });
